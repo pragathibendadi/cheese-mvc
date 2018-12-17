dc5ea4502d4a987b115855a68f40d4526b10e493
pragathibendadi <43053009+pragathibendadi@users.noreply.github.com>
2018-12-17 01:10:36 -0600

    Add files via upload

--- cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static  ArrayList<String> cheeses = new ArrayList<>();
+    //static  ArrayList<String> cheeses = new ArrayList<>();
+   static HashMap<String,String>cheeses =new HashMap<>();
 
     // Request path : /cheese
     @RequestMapping(value = "")
@@ -36,9 +38,24 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+         cheeses.put(cheeseName,cheeseDescription);
+        //cheeses.add(cheeseName);
 
-          cheeses.add(cheeseName);
-          return "redirect:";
+          return "redirect:";//redirect to /cheese
+    }
+    @RequestMapping(value = "remove" ,method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model){
+        model.addAttribute("cheeses",cheeses.keySet());
+        model.addAttribute("title", "Remove Cheese");
+
+        return "cheese/remove";
+    }
+    @RequestMapping(value = "remove" ,method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam ArrayList<String> cheese){
+       for (String  aCheese : cheese){
+           cheeses.remove(aCheese);
+       }
+       return "redirect:";
     }
 }
